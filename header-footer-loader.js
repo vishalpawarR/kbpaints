8c59b747ad9a0fa0c2d1bdfb3b1958c40048656f
Vishal Pawar <vishalpawarr.git@gmail.com>
2024-10-09 17:54:48 +0530

    feat: added the active menu option

--- header-footer-loader.js
+++ header-footer-loader.js
@@ -1,11 +1,22 @@
 const header = document.getElementById('header');
 const footer = document.getElementById('footer');
+const path = window.location.pathname;
+console.log(path);
 
 function loadHeaderFooter() {
   fetch('./components/header.html')
     .then((response) => response.text())
     .then((data) => {
       header.innerHTML = data;
+      const navLink = document.querySelectorAll('.navLink');
+
+      console.log(navLink);
+      navLink.forEach((ele) => {
+        const hrefRef = ele.getAttribute('href');
+        if (hrefRef === path) {
+          ele.classList.add('active');
+        }
+      });
     });
 
   fetch('./components/footer.html')
